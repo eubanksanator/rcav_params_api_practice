9b2b16e736f320e61bf0202088ab09bd0c2c68ff
Arjun Venkataswamy <arjun@example.com>
2015-01-29 12:53:40 -0600

    starting point

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get("/pirate/:sentence", {:controller => "pages", :action => "pirate"})
-  get("/image/:search", {:controller => "pages", :action => "image"})
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
